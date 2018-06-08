538946839cfad7ef8d6cb104c9bb5ae50a462053
Empresa <empresa@aulas.local>
2018-06-08 10:15:57 +0200

    Upload1

---
+++ js/home.js
@@ -0,0 +1,17 @@
+function myMap() {
+  var pos = {lat: 41.361142, lng: 1.9863898};
+  var map = new google.maps.Map(document.getElementById('mapFooter'), {
+    zoom: 16,
+    center: pos,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
+  var marker = new google.maps.Marker({
+    position: pos,
+    map: map
+  });
+}
+
+
+
+
+			
